514d270961600d8cbbc26f0f78caf7f4d3dc0dfb
Yeeun Lee <leeyeah8245@gmail.com>
2022-07-14 17:55:44 +0900

    initial commit

---
+++ app/src/main/java/kr/co/kumoh/d134/isl/view/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.kumoh.d134.isl.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.kumoh.d134.isl.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
