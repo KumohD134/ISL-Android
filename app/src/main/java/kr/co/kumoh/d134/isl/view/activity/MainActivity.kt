13561b392fbb02b86e6081d9818cac57ea4020fa
Yeeun Lee <leeyeah8245@gmail.com>
2022-07-26 19:48:54 +0900

    connect actionbar nav

--- app/src/main/java/kr/co/kumoh/d134/isl/view/activity/MainActivity.kt
+++ app/src/main/java/kr/co/kumoh/d134/isl/view/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.kumoh.d134.isl.view.activity
 import android.util.Log
 import android.view.Gravity
 import androidx.activity.viewModels
+import androidx.appcompat.widget.Toolbar
 import kr.co.kumoh.d134.isl.R
 import kr.co.kumoh.d134.isl.base.BaseActivity
 import kr.co.kumoh.d134.isl.base.ResponseResult
@@ -15,22 +16,48 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun getLayoutRes(): Int = R.layout.activity_main
 
     override fun subscribeUi() {
-        with(mViewModel) {
-
-        }
+//        mDataBinding.constraint.setOnClickListener {
+//            Log.d("터치","터치")
+//        }
+//        with(mViewModel) {
+//
+//        }
         with(mDataBinding) {
             vm = mViewModel
-            drawerLayout.closeDrawers()
-            btnDrawer.setOnClickListener { changeDrawer() }
+            val toolbar = findViewById<Toolbar>(R.id.toolbar)
+            setSupportActionBar(toolbar)
+            val actionBar = supportActionBar
+            actionBar?.apply {
+                Log.d("동작 확인"," 툴바")
+                setDisplayShowCustomEnabled(true)
+                setDisplayShowTitleEnabled(false)
+                setDisplayHomeAsUpEnabled(true)
+            }
+            //changeDrawer()
+            //drawerLayout.closeDrawers()
+            toolbar.setNavigationOnClickListener {
+                Log.d("서랍 엶1","작동")
+                changeDrawer()
+            }
         }
+    }
+
+    override fun onBackPressed() {
+        if (mDataBinding.drawerLayout.isDrawerOpen(Gravity.LEFT))
+            mDataBinding.drawerLayout.closeDrawer(Gravity.LEFT)
+        else    // TODO: 초 시간 정의
+            super.onBackPressed()
     }
 
     fun changeDrawer(){
         Log.d("서랍 엶","작동")
         if(!mDataBinding.drawerLayout.isDrawerOpen(Gravity.LEFT)){
+            Log.d("서랍 엶","확인1")
             mDataBinding.drawerLayout.openDrawer(Gravity.LEFT)
-        }else
+        }else {
+            Log.d("서랍 엶","확인2")
             mDataBinding.drawerLayout.closeDrawer(Gravity.LEFT)
+        }
     }
 
     override fun loadResultCode(resResult: ResponseResult?) {
