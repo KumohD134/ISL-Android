ce88da885899200bfa963e83fcff353b85422511
Yeeun Lee <leeyeah8245@gmail.com>
2022-07-27 19:00:38 +0900

    set fragment of main

---
+++ app/src/main/java/kr/co/kumoh/d134/isl/view/activity/AuthActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.kumoh.d134.isl.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
