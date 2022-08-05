ffd1d7fe33bd5a157603a952dd08d242f300ec7e
Yeeun Lee <leeyeah8245@gmail.com>
2022-08-05 19:55:18 +0900

    [Add codes] set compose base

--- app/src/main/java/kr/co/kumoh/d134/isl/view/fragment/GalleryFragment.kt
+++ app/src/main/java/kr/co/kumoh/d134/isl/view/fragment/GalleryFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import kr.co.kumoh.d134.isl.R
 
 class GalleryFragment : Fragment() {
@@ -12,9 +15,22 @@ class GalleryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                showScreen()
+            }
+        }
     }
+}
+
+@Composable
+fun showScreen(){
+
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun showScreenPV(){
 
 }
